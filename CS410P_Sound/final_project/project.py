47766205b2dd9498e6cf2cc5f288a958b9a017de
Ebraheem AlAthari <23537510+plavabe12@users.noreply.github.com>
2019-05-16 11:39:23 -0700

    Add Functions to the project file

--- CS410P_Sound/final_project/project.py
+++ CS410P_Sound/final_project/project.py
@@ -1,3 +1,33 @@
 """
 Final Project for CS410P
+By: Ebraheem AlAthari and Medina Lamkin
+Date: 05/16/2019
 """
+
+# Possible Functions
+def compression():
+    pass
+
+def tremolo():
+    pass
+
+def vibration():
+    pass
+
+# Functions to work on
+def echo():
+    pass
+
+def reverb():
+    pass
+
+def normalization():
+    pass
+
+# Main Functions
+# The Start of the program
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
