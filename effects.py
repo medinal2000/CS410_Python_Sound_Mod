a9f99255f59dbf1a4c23789ec460fe8108b2e969
plavabe12 <23537510+plavabe12@users.noreply.github.com>
2019-06-04 17:38:32 -0700

    Fixed the Echo function and it can import in seconds now

--- effects.py
+++ effects.py
@@ -28,7 +28,7 @@ class Effects:
         name = filename.split('.')
         self.name = name[0]
         # used for echo functions
-        # TODO try to move this to the echo function
+        # NOTE needs to be here in order to be used by the echo function
         self.echos_left = []
         self.echos_right = []
         # dictionary to map each effects function to the number they
@@ -60,7 +60,6 @@ class Effects:
         pass
 
     def normalization(self):
-        print("normalization")
         self.left = self.normalize_channel(self.left)
         self.right = self.normalize_channel(self.right)
 
@@ -87,32 +86,44 @@ class Effects:
         channel = channel_delay
         return channel
 
-    def echo(self, num_of_echos=4):
-        self.echo_function(num_of_echos)
+    def echo(self, num_of_echos=4, duration_of_echo=1):
+        self.num_of_echos = num_of_echos
+        self.duration_of_echo = duration_of_echo
+        self.valid_echo()
+        self.echo_function()
         pass
 
-    def echo_function(self,num_of_echos):
-        if num_of_echos < 1:
-            print('Number of Echos was recvived as a negative number')
-            print('Setting Number of Echos to 4')
-            num_of_echos = 4
-            pass
-        elif num_of_echos > self.MAX_ECHO:
-            print('Number of Echos was recvived as a number higher then 7')
-            print('Setting Number of Echos to 7')
-            num_of_echos = self.MAX_ECHO
+    def echo_function(self):
+        duration = self.duration_of_echo * 1000
+        for i in range(0,self.num_of_echos):
+            self.echos_left.insert(i,self.audio_delay_channel(self.original_left,duration*(i+1)))
+            self.echos_right.insert(i,self.audio_delay_channel(self.original_right,duration*(i+1)))
             pass
 
-        for i in range(0,num_of_echos):
-            self.echos_left.insert(i,self.audio_delay_channel(self.original_left,1000*(i+1)))
-            self.echos_right.insert(i,self.audio_delay_channel(self.original_right,1000*(i+1)))
-            pass
-
-        for i in range(0,num_of_echos):
+        for i in range(0,self.num_of_echos):
             self.left = numpy.resize(self.left,self.echos_left[i].shape[0])
             self.right = numpy.resize(self.right,self.echos_right[i].shape[0])
             self.left = numpy.add(self.left,self.echos_left[i])
             self.right = numpy.add(self.right,self.echos_right[i])
+            pass
+        pass
+
+    def valid_echo(self):
+        if self.num_of_echos < 1:
+            print('Number of Echos was recvived as a negative number')
+            print('Setting Number of Echos to 4')
+            self.num_of_echos = 4
+            pass
+        elif self.num_of_echos > self.MAX_ECHO:
+            print('Number of Echos was recvived as a number higher then 7')
+            print('Setting Number of Echos to 7')
+            self.num_of_echos = self.MAX_ECHO
+            pass
+
+        if self.duration_of_echo < 1:
+            print('Duration of Echos was recvived less than a second')
+            print('Setting Duration of Echos to 1 second')
+            self.duration_of_echo = 1
             pass
         pass
 
